b241fb03003502258ea84eb066ae432d5fb3a329
leesymbol_laptop <leesymbol@163.com>
2014-12-01 23:29:39 +0800

    NGUITest with sequence frame

---
+++ NGUITest/Assets/ChangeTexture.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ChangeTexture : MonoBehaviour
+{
+
+
+		private UITexture myTexture;
+		private float speed = 1f;
+		private Texture2D[] textureResources;
+		private int index = 0;
+		public int effeciency = 25;
+		private float count = 3f;
+		private float temp = 0f;
+		private bool ifPlay = false;
+
+		void Start ()
+		{
+				myTexture = transform.GetComponent<UITexture> ();
+				textureResources = (Texture2D[])Resources.LoadAll ("Texture/Traffic", typeof(Texture2D)).Cast<Texture2D> ().ToArray ();
+				;
+		}
+
+		void Update ()
+		{
+				if (textureResources == null)
+						return;
+				if (!ifPlay)
+						return;
+				temp += Time.deltaTime;
+		
+				if (temp >= 1.0 / effeciency) {
+
+						index++;
+			
+						temp = 0;
+
+						if (index >= textureResources.Length - 1) {
+								index = textureResources.Length - 1;
+							
+						}
+				}
+
+				myTexture.mainTexture = textureResources [index];
+		}
+
+		void OnGUI ()
+		{
+				if (GUILayout.Button ("Play")) {
+						
+								ifPlay = true;
+				}
+		}
+	public void Event()
+	{
+		print ("Click !");
+	}
+}
